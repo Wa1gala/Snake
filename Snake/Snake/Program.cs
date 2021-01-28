f7d9c59f1e43d6359a897367756219c43d369f5d
Walgala <zaquz111@gmail.com>
2021-01-28 15:37:01 +0300

    Процедурный подход, функция Drow.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,8 +6,24 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int x1 = 1;
+            int y1 = 4;
+            char sym1 = '*';
+
+            Drow(x1, y1, sym1);
+
+            int x2 = 4;
+            int y2 = 5;
+            char sym2 = '#';
+
+            Drow(x2,y2,sym2);
+
             Console.ReadLine();
+        }
+        static void Drow(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
