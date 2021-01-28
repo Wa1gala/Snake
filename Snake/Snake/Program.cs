5239ce06fddcc1cd8e17639768d1bd4a191bd248
Walgala <zaquz111@gmail.com>
2021-01-28 17:11:15 +0300

    Конструкторы. Инкапсуляция

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,18 +6,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 3;
-            p1.y = 5;
-            p1.sym = '#';
-
+            Point p1 = new Point(3, 5, '#');
             p1.Drow();
 
-            Point p2 = new Point();
-            p2.x = 6;
-            p2.y = 8;
-            p2.sym = '*';
-
+            Point p2 = new Point(6, 8, '*');
             p2.Drow();
        
             Console.ReadLine();
