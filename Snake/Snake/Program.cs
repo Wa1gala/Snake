b450d91e70d9b05e02ff5955679b19b8ee02a295
Walgala <zaquz111@gmail.com>
2021-01-29 19:45:16 +0300

    !

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -13,6 +13,12 @@ namespace Snake
             Point p2 = new Point(6, 8, '*');
             p2.Drow();
 
+            Point p3 = new Point(9, 4, '&');
+            p3.Drow();
+
+            Point p4 = new Point(11, 5, '`');
+            p4.Drow();
+
             List<int> numList = new List<int>();
             numList.Add(0);
             numList.Add(1);
@@ -31,6 +37,17 @@ namespace Snake
             pList.Add(p1);
             pList.Add(p2);
 
+            List<char> charList = new List<char>();
+            charList.Add('~');
+            charList.Add('@');
+
+            foreach(char i in charList)
+            {
+                Console.WriteLine(i);
+            }
+
+            charList.RemoveAt(1);
+    
             Console.ReadLine();
         }
     }
